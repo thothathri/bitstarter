1a026b5497064f0adcaba8fcb8660eac452e8083
Thothathri Srinivasan <me@thothathri.com>
2013-07-10 16:04:32 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var resp = fs.readFileSync('./index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
